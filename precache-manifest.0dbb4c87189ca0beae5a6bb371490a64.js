c8a92b5d1b3f3df2814dc6d81af2717b8f89eb2a
okyska <okyska@yahoo.com>
2019-12-22 17:08:37 -0800

    Updates

---
+++ precache-manifest.0dbb4c87189ca0beae5a6bb371490a64.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "107ae7d16b275f5e99ce6fdae3f86e89",
+    "url": "/furn/index.html"
+  },
+  {
+    "revision": "9478b367c1f37d6d4d51",
+    "url": "/furn/static/css/main.3c3018bb.chunk.css"
+  },
+  {
+    "revision": "3f4299fb8604bfee622b",
+    "url": "/furn/static/js/2.26fda01a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/furn/static/js/2.26fda01a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9478b367c1f37d6d4d51",
+    "url": "/furn/static/js/main.0cc96f08.chunk.js"
+  },
+  {
+    "revision": "ff3321af49bbb1850a3e",
+    "url": "/furn/static/js/runtime-main.4c45bd94.js"
+  },
+  {
+    "revision": "7b561ea674abb52d8fb9fdeabfda6f00",
+    "url": "/furn/static/media/1.7b561ea6.png"
+  },
+  {
+    "revision": "b5123d989d9ac26fda9c9986262764ed",
+    "url": "/furn/static/media/2.b5123d98.png"
+  },
+  {
+    "revision": "60264d7bb4be694da88fc5d3b7e86008",
+    "url": "/furn/static/media/3.60264d7b.png"
+  },
+  {
+    "revision": "6162814411498694ee9c89a2790b487e",
+    "url": "/furn/static/media/4.61628144.png"
+  },
+  {
+    "revision": "48c83317fd7d1ee0188185d9fccb5d11",
+    "url": "/furn/static/media/5.48c83317.png"
+  },
+  {
+    "revision": "4e872dccaceaed089a8d6d152b1ad51e",
+    "url": "/furn/static/media/6.4e872dcc.png"
+  },
+  {
+    "revision": "ac76f3e9713ce297a798ac6852f5342c",
+    "url": "/furn/static/media/7.ac76f3e9.png"
+  },
+  {
+    "revision": "2dcb8ba684bdf0fca8a67930c8cf5e25",
+    "url": "/furn/static/media/8.2dcb8ba6.jpg"
+  },
+  {
+    "revision": "f44bc7e56647dfd36d32f4554e6f516a",
+    "url": "/furn/static/media/9.f44bc7e5.png"
+  },
+  {
+    "revision": "53dd5c606eaee3a6e2e18452cc3dfa57",
+    "url": "/furn/static/media/cart1.53dd5c60.png"
+  },
+  {
+    "revision": "ff2e33e1fe86ceb3ec331f53299d71f5",
+    "url": "/furn/static/media/cart2.ff2e33e1.png"
+  },
+  {
+    "revision": "5a893c313d95e0cf80fb806842f140a7",
+    "url": "/furn/static/media/cart3.5a893c31.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/furn/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/furn/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/furn/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/furn/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/furn/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "f302e7d728e248b7d417dbacc35c199a",
+    "url": "/furn/static/media/helvetica_neu_bold-webfont.f302e7d7.woff2"
+  },
+  {
+    "revision": "f9c3c1802f72242572610edc99e9bf7b",
+    "url": "/furn/static/media/helvetica_neu_bold-webfont.f9c3c180.woff"
+  },
+  {
+    "revision": "05288832f58997a646f8c2fda25353fd",
+    "url": "/furn/static/media/helveticaneue_medium-webfont.05288832.woff2"
+  },
+  {
+    "revision": "52292e7a56c7f35bc38c07aa226b3526",
+    "url": "/furn/static/media/helveticaneue_medium-webfont.52292e7a.woff"
+  }
+]);
